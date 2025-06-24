c0b73beec52bc6da3019286a0d7503e59051054e
Mark <mark@remarkablemark.org>
2025-06-23 22:10:47 -0400

    refactor(constants): move constants to index.ts
    Release-As: 1.0.7

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,15 @@ import { addPath, getInput, setFailed } from '@actions/core';
 import { exec } from '@actions/exec';
 import { cacheFile, downloadTool, extractZip } from '@actions/tool-cache';
 
-import { CLI_NAME, VERSION } from './constants';
 import { getBinaryPath, getDownloadUrl } from './utils';
+
+const DEFAULT_NAME = 'butler';
 
 export async function run() {
   try {
     // Get the version of the tool to be installed
-    const version = getInput('butler-version') || VERSION;
-    const name = getInput('cli-name') || CLI_NAME;
+    const version = getInput('butler-version');
+    const name = getInput('cli-name') || DEFAULT_NAME;
 
     // Download the specific version of the tool (e.g., tarball/zipball)
     const downloadUrl = getDownloadUrl(version);
@@ -20,8 +21,11 @@ export async function run() {
 
     // Rename the binary
     const binaryPath = getBinaryPath(extractDirectory, name);
-    if (name !== CLI_NAME) {
-      await exec('mv', [getBinaryPath(extractDirectory, CLI_NAME), binaryPath]);
+    if (name !== DEFAULT_NAME) {
+      await exec('mv', [
+        getBinaryPath(extractDirectory, DEFAULT_NAME),
+        binaryPath,
+      ]);
     }
 
     // Cache the tool
